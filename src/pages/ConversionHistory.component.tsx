0d054ffe7e0a5d1a0fd737501a62fbc1743f6b40
tsloniowski <tomasz.sloniowski@dxc.com>
2022-10-31 08:35:55 +0100

    improvements

--- src/pages/ConversionHistory.component.tsx
+++ src/pages/ConversionHistory.component.tsx
@@ -1,3 +1,5 @@
+import arrowIcon from "../assets/arrow_right.svg";
+
 interface ConversionHistoryProps {
   data: any[];
 }
@@ -8,20 +10,26 @@ export const ConversionHistory: React.FC<ConversionHistoryProps> = (props) => {
       <table>
         <thead>
           <tr>
-            <th style={{width: '680px'}}>Data</th>
-            <th style={{width: '200px'}}>Przed konwersją</th>
-            <th style={{width: '180px', textAlign: 'end'}}>Po konwersji</th>
+            <th style={{minWidth: '550px'}}>Data</th>
+            <th style={{minWidth: '150px'}}>Przed konwersją</th>
+            <th style={{minWidth: '270px', textAlign: 'end'}}>Po konwersji</th>
           </tr>
         </thead>
         <tbody>
           {props.data.map((row) => (
             <tr>
-              <td style={{width: '770px'}}>{row.date}</td>
+              {/* <td style={{width: '670px'}}>{row.date}</td> */}
+              <td style={{width: '750px'}}>23.10.2022</td> {/*mock'*/}
+
               <td>
                 {`${row.amount} ${row.from}`}
               </td>
-              <td style={{textAlign: 'end', fontWeight: 600, marginRight: '10px'}}>
-              {`${row.result} ${row.to}`}
+              <td style={{width: '30px', minWidth: '30px'}}>
+                <img src={arrowIcon} alt="arrow" />
+              </td>
+              <td style={{textAlign: 'end', fontWeight: 600}}>
+              {/* {`${row.result} ${row.to}`} */}
+              {`${row.result} USD`} {/*mock'*/}
               </td>
             </tr>
           ))}
--- src/pages/Converter.component.tsx
+++ src/pages/Converter.component.tsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
-import { useQuery } from "react-query";
 import { convertCurrency, getAllCurrencies } from "../converter.service";
 import "../styles.css";
 import { ConversionHistory } from "./ConversionHistory.component";
+import { currency_list } from "../CurrenciesMock";
+import coi_logo from "../assets/coi_logo.svg";
+import arrows from "../assets/arrows.svg";
+import close_icon from "../assets/close_icon.svg";
 
 type FormData = {
   amount: number;
@@ -15,99 +18,57 @@ export default function Converter() {
   const [currencies, setCurrencies] = useState<any[]>([]);
   const [conversionHistory, setConversionHistory] = useState<any[]>([]);
   const [isConversionHistoryVisible, setIsConversionHistoryVisible] = useState<boolean>(false);
-  const [selectedFromCurrency, setSelectedFromCurrency] = useState<string>("USD");
-  const [selectedToCurrency, setSelectedToCurrency] = useState<string>("PLN");
-
-  // const conversionHistoryMock = [
-  //   {
-  //     date: "03.03.2020",
-  //     amount: "385",
-  //     result: "98,56",
-  //     from: "USD",
-  //     to: "PLN",
-  //   },
-  //   {
-  //     date: "03.03.2020",
-  //     amount: "385",
-  //     result: "98,56",
-  //     from: "USD",
-  //     to: "PLN",
-  //   },
-  //   {
-  //     date: "03.03.2020",
-  //     amount: "385",
-  //     result: "98,56",
-  //     from: "USD",
-  //     to: "PLN",
-  //   },
-  //   {
-  //     date: "03.03.2020",
-  //     amount: "385",
-  //     result: "98,56",
-  //     from: "USD",
-  //     to: "PLN",
-  //   },
-  //   {
-  //     date: "03.03.2020",
-  //     amount: "385",
-  //     result: "98,56",
-  //     from: "USD",
-  //     to: "PLN",
-  //   },
-  //   {
-  //     date: "03.03.2020",
-  //     amount: "385",
-  //     result: "98,56",
-  //     from: "USD",
-  //     to: "PLN",
-  //   },
-  //   {
-  //     date: "03.03.2020",
-  //     amount: "385",
-  //     result: "98,56",
-  //     from: "USD",
-  //     to: "PLN",
-  //   },
-  // ];
+  const [selectedFromCurrency, setSelectedFromCurrency] = useState<string>("");
+  const [selectedToCurrency, setSelectedToCurrency] = useState<string>("");
+  const [isAlertModal, setIsAlertModal] = useState<boolean>(true);
 
   const {
     register,
     setValue,
     handleSubmit,
+    watch,
     formState: { errors },
   } = useForm<FormData>();
+  const watchShowAge = watch("amount");
 
   const onSubmit = handleSubmit(({ amount, fromCurrency, toCurrency }) => {
-    console.log(amount, fromCurrency, toCurrency);
     convertCurrency(amount, fromCurrency, toCurrency)
       .then((response) => response.text())
       .then((result) => {
         const res = JSON.parse(result);
-        console.log(res);
         setValue("result", res.result);
-        const row = { date: res.date, amount: amount, result: res.result, from: fromCurrency, to: toCurrency };
+        const row = { date: res.date, amount: amount, res, from: fromCurrency, to: toCurrency };
         setConversionHistory((state) => [...state, row]);
         setIsConversionHistoryVisible(true);
         setSelectedFromCurrency(fromCurrency);
         setSelectedToCurrency(toCurrency);
       })
-      .catch((error) => console.log("error", error));
+      .catch((error) => {
+        setIsAlertModal(true);
+        console.log("error", error);
+      });
   });
 
+  const symbols = currency_list.map((s) => s.code);
+
+  // useEffect(() => {
+  //   getAllCurrencies()
+  //     .then((response) => response.text())
+  //     .then((result) => {
+  //       const res = JSON.parse(result);
+  //       setCurrencies(res);
+  //     })
+  //     .catch((error) => console.log("error", error));
+  // }, []);
+
   useEffect(() => {
-    getAllCurrencies()
-      .then((response) => response.text())
-      .then((result) => {
-        const res = JSON.parse(result);
-        setCurrencies(res);
-        console.log(res);
-      })
-      .catch((error) => console.log("error", error));
+    setValue("fromCurrency", "PLN");
+    setValue("toCurrency", "USD");
   }, []);
 
   return (
-    <div className="container">
-      {currencies && (
+    <>
+      {symbols && (
         <div className="form-tab">
           <h1>Konwerter walut</h1>
           <form onSubmit={onSubmit}>
@@ -115,15 +76,18 @@ export default function Converter() {
               <div className="input-with-label">
                 <label>Przelicz z</label>
                 <select {...register("fromCurrency")}>
-                  {currencies.map((option) => (
+                  {symbols.map((option) => (
                     <option value={option}>{option}</option>
                   ))}
                 </select>
               </div>
-              <div className="input-with-label">
+              <div>
+                <img src={arrows} alt="strzałki" className="arrows" />
+              </div>
+              <div className="input-with-label" style={{ marginRight: "40px" }}>
                 <label>Przelicz na</label>
                 <select {...register("toCurrency")}>
-                  {currencies.map((option) => (
+                  {symbols.map((option) => (
                     <option value={option}>{option}</option>
                   ))}
                 </select>
@@ -131,17 +95,25 @@ export default function Converter() {
               <div className="input-with-label">
                 <label htmlFor="amount">Kwota</label>
                 <input
+                  type="text"
                   placeholder="Wpisz kwotę"
-                  style={{ width: "250px", borderBottom: "solid 2px #335566", borderRadius: "0px" }}
-                  {...register("amount", { required: true })}
+                  style={{
+                    width: "200px",
+                    color: errors.amount ? "#CA163A" : "#335566",
+                    borderBottom: !errors.amount ? "solid 2px #335566" : "solid 3px #CA163A",
+                    borderRadius: "0px",
+                  }}
+                  {...register("amount", { pattern: /\d+/ })}
                 />
+                {errors.amount && <p className="error-message">Nieprawidłowa wartość</p>}
                 <div className="symbol">{selectedFromCurrency}</div>
               </div>
               <div className="input-with-label">
                 <label htmlFor="output">Wynik</label>
                 <input
+                  type="number"
                   placeholder="Wynik"
-                  style={{ width: "250px", borderBottom: "solid 2px #335566", borderRadius: "0px" }}
+                  style={{ width: "200px", borderBottom: "solid 2px #335566", borderRadius: "0px" }}
                   {...register("result")}
                 />
                 <div className="symbol">{selectedToCurrency}</div>
@@ -157,7 +129,7 @@ export default function Converter() {
               >
                 {!isConversionHistoryVisible ? "Historia" : "Ukryj historię"}
               </button>
-              <button className="button-convert" type="button" onClick={onSubmit}>
+              <button className="button-convert" type="button" onClick={onSubmit} disabled={!watchShowAge}>
                 Konwertuj
               </button>
             </div>
@@ -165,6 +137,20 @@ export default function Converter() {
           {isConversionHistoryVisible && <ConversionHistory data={conversionHistory} />}
         </div>
       )}
-    </div>
+      <div className="logo">
+        <img src={coi_logo} alt="logo" />
+      </div>
+      {isAlertModal && (
+        <>
+          <div className="alert-modal-backdrop"></div>
+          <div className="alert-modal">
+            <div className="alert-bar"></div>
+            <h1>Komunikat błędu</h1>
+            <img src={close_icon} className="close-icon" alt="zamknij" onClick={() => setIsAlertModal(false)} />
+            <p>Nie udało się wykonać żądanej operacji, ponieważ nie znaleziono zasobu powiązanego z żądaniem.</p>
+          </div>
+        </>
+      )}
+    </>
   );
 }
